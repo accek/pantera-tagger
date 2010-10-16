ab6d8850351a52a345d74fe3075628e05458b1a7
s.accek <s.accek@df3ffdb3-f59c-f0aa-476a-5972c8d6f237>
2010-10-16 14:45:54 +0000

    augment default Polish segmentation disambiguation dictionary

--- src/nlpcommon/polish_segm_disamb.cpp
+++ src/nlpcommon/polish_segm_disamb.cpp
@@ -3,10 +3,14 @@
 namespace NLPCommon {
 
 const wchar_t* default_polish_segm_disamb_config =
-    L"piekłem separate\n"
+    L"baliście separate\n"
     L"czekał.m separate\n"
+    L"kulturalno-oświatowy.* separate\n"
     L"miałem separate\n"
+    L"piekłem separate\n"
+    L"podziałom together\n"
     L"winnym together\n"
+    L"wyłom together\n"
     L".*łem together\n"
     L".*ś together\n"
     ;
