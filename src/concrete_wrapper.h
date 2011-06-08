25477a5959e86caf4efb03933876986fdc093a31
s.accek <s.accek@df3ffdb3-f59c-f0aa-476a-5972c8d6f237>
2011-06-08 20:20:59 +0000

    added files missing from previous commit

---
+++ src/concrete_wrapper.h
@@ -0,0 +1,62 @@
+/*
+ * ConcretePanteraWrapper.h
+ *
+ *  Created on: 26-05-2011
+ *      Author: bz
+ */
+
+#ifndef CONCRETEPANTERAWRAPPER_H_
+#define CONCRETEPANTERAWRAPPER_H_
+
+#include <vector>
+#include <string>
+#include <nlpcommon/tag.h>
+#include <nlpcommon/lexeme.h>
+#include "wrapper.h"
+
+#include <nlpcommon/lexer.h>
+#include <nlpcommon/plaintextlexer.h>
+#include <nlpcommon/lexeme.h>
+#include <nlpcommon/lexemesfilter.h>
+#include <nlpcommon/spejdtagsetloader.h>
+#include <nlpcommon/libsegmentsentencer.h>
+#include <nlpcommon/morfeusz.h>
+#include <nlpcommon/tagset.h>
+#include <nlpcommon/polish_segm_disamb.h>
+#include <nlpcommon/util.h>
+#include <nlpcommon/ipipanwriter.h>
+
+#include "pantera.h"
+#include "pantera_rules.h"
+
+namespace Pantera {
+
+using namespace std;
+using namespace NLPCommon;
+
+typedef BTagger::BrillLexeme<Tag> MyLexeme;
+typedef BestScoreMultiGoldenScorer<BinaryScorer<MyLexeme::tag_type> > MyScorer;
+
+class ConcretePanteraWrapper : public PanteraWrapper {
+public:
+	virtual std::vector<NLPCommon::DefaultLexeme> tag(const std::string& text);
+
+	ConcretePanteraWrapper(string enginePath, string tagsetName);
+	virtual ~ConcretePanteraWrapper();
+	virtual std::vector<NLPCommon::DefaultLexeme> tag(const
+      std::vector<NLPCommon::DefaultLexeme>& lexems, bool doSentSplit,
+      bool doMorphAnalysis, bool doSegmentDisamb, bool doTagging);
+
+private:
+  void morphAnalyze(vector<MyLexeme>& lexems);
+  void segmentDisamb(vector<MyLexeme>& lexems);
+
+  MorfeuszAnalyzer<MyLexeme> morfeusz;
+  PolishSegmDisambiguator<MyLexeme> segm_disamb;
+  BTagger::BrillEngine<MyLexeme, MyScorer> engine;
+  std::vector<BTagger::RulesGenerator<MyLexeme>*> rule_generators;
+
+};
+}
+
+#endif /* CONCRETEPANTERAWRAPPER_H_ */
